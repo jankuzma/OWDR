a7b3cf6b70880e27e25aae8338fb4d20eb93a17c
jankuzma <1jankuzma@gmail.com>
2023-09-04 18:58:49 +0200

    html setup

--- backend_core/home/views.py
+++ backend_core/home/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = 'home/home.html'
+    template_name = 'index.html'
+
+
+class FormView(TemplateView):
+    template_name = 'form.html'
