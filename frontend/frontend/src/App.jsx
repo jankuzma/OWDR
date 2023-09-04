bb2a3db450a627a5d5671cdd456ffbeb524f84f5
jankuzma <1jankuzma@gmail.com>
2023-09-04 17:56:29 +0200

    init

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+            HELLO WORLD
+      </div>
+    </>
+  )
+}
+
+export default App
